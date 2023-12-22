64cd3cd04010764ae4e060fb61882303a518be3f
ecschiullo <edmarcsc23@hotmail.com>
2023-12-22 09:48:57 -0300

    //

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import '../style/index.css';
 import { utils, read, writeFileXLSX } from "xlsx";
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set, onValue, get, remove } from "firebase/database";
-
-// Key acess hosting and database
-const firebaseConfig = {
-  apiKey: "AIzaSyB0IV0RAdqkE-TqR7scVzzaSXy8KfFFQc4",
-  authDomain: "topull-c7f63.firebaseapp.com",
-  projectId: "topull-c7f63",
-  storageBucket: "topull-c7f63.appspot.com",
-  messagingSenderId: "918543856050",
-  appId: "1:918543856050:web:7f867159815a1b4c3df4ab",
-  measurementId: "G-8V9JR8P40D"
-};
-
+import firebaseConfig from './apiKey'
 // initial app
 const app = initializeApp(firebaseConfig);
 // DB
